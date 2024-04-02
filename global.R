25f59451ed1e2f1b6a001681318c12c488e8c400
Gaurav P. Malpedi <gaurav.malpedi17@gmail.com>
2024-04-02 21:40:10 +0530

    second commit

--- global.R
+++ global.R
@@ -1,5 +1,4 @@
 
-# Import relevant libraries
 library(rsconnect)
 library(dplyr)
 library(ggplot2)
@@ -10,8 +9,6 @@ library(shinyWidgets)
 library(RColorBrewer)
 #library(ColorPalette)
 
-# Set working directory 
-#setwd("C:/Users/Guillermo/Desktop/ESTUDIOS/Data Science Bootcamp/Projects/Shiny/CustomerSegmentation")
 df <- read.csv('supermarket_sales.csv')
 
 # Chage column names to more adequate ones plus make column names lower case
